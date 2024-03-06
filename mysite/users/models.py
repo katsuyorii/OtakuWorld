083aaefdd0a9eb5175f739fc436a360f2ee179a7
katsuyorii <katsuyoriiii@gmail.com>
2024-03-06 11:36:25 +0300

    start project and overrate user model

---
+++ mysite/users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
+
+class User(AbstractUser):
+    email = models.EmailField(verbose_name='Email', unique=True)
+    username = models.CharField('Имя пользователя', max_length=100)
+    image = models.ImageField(verbose_name='Изображение', upload_to='users_profile_images', null=True, blank=True)
+    phone_number = PhoneNumberField(verbose_name='Номер телефона', null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+
