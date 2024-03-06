0b5221c757aaeb5c5817d2da65bc3c2389899e9b
katsuyorii <katsuyoriiii@gmail.com>
2024-03-06 13:31:41 +0300

    add category model and create catalog page

--- mysite/catalog/models.py
+++ mysite/catalog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from pytils.translit import slugify
 
-# Create your models here.
+# Модель для категорий товаров (Манга, канцелярия, одежда)
+class Category(models.Model):
+    name = models.CharField(verbose_name='Наименование', max_length=128, unique=True)
+    slug = models.SlugField(verbose_name='Слаг', max_length=128, unique=True, db_index=True, editable=False)
+    icon_image = models.ImageField(verbose_name='Изображение категории', upload_to='icons_categories')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    '''
+    Переопределение метода сохранения записи.
+    С помощью библиотеки pytils переводим киррилицу в латиницу по полю name.
+    В поле slug записывается поле name на латинице.
+    '''
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Category
 
-# Create your views here.
+# Класс-представления каталога категорий
+class CatalogView(ListView):
+    model = Category
+    template_name = 'catalog/catalog.html'
+    context_object_name = 'categories'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Каталог товаров'
+
+        return context
+
