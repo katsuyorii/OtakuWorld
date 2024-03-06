f01e94daae1b518a40cd8bed830a1443aedff42f
katsuyorii <katsuyoriiii@gmail.com>
2024-03-06 12:51:49 +0300

    add all core pages

--- mysite/core/urls.py
+++ mysite/core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, DeliveryView, RefundView, AboutView, ContactsView
 
 urlpatterns = [
-    path('',IndexView.as_view(), name='index'),
+    path('', IndexView.as_view(), name='index'),
+    path('delivery/', DeliveryView.as_view(), name='delivery'),
+    path('refund/', RefundView.as_view(), name='refund'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
 ]
--- mysite/core/views.py
+++ mysite/core/views.py
@@ -9,5 +9,47 @@ class IndexView(TemplateView):
         context['title'] = 'Аниме магазин OTAKU'
 
         return context
-    
 
+
+# Класс-представление для страницы "Оплата и доставка"
+class DeliveryView(TemplateView):
+    template_name = 'core/delivery.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Оплата и доставка'
+
+        return context
+
+
+# Класс-представление для страницы "Возврат и обмен"
+class RefundView(TemplateView):
+    template_name = 'core/refund.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Возврат и обмен'
+
+        return context
+
+
+# Класс-представление для страницы "О нас"
+class AboutView(TemplateView):
+    template_name = 'core/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'О нас'
+
+        return context
+
+
+# Класс-представление для страницы "Контакты"
+class ContactsView(TemplateView):
+    template_name = 'core/contacts.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Контакты'
+
+        return context
