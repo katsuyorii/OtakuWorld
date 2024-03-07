8b38e9e80f0234149c1519457763cc3dfb12eb52
katsuyorii <katsuyoriiii@gmail.com>
2024-03-07 20:17:58 +0300

    add products and property models

--- mysite/catalog/admin.py
+++ mysite/catalog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Genre, Source, Product, Property, ProductProperty
 
 admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Source)
+admin.site.register(Product)
+admin.site.register(Property)
+admin.site.register(ProductProperty)
--- mysite/catalog/models.py
+++ mysite/catalog/models.py
@@ -22,3 +22,109 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# Модель для жанров (манга, манхва, маньхуа и т.д.)
+class Genre(models.Model):
+    name = models.CharField(verbose_name='Наименование', max_length=128, unique=True)
+    slug = models.SlugField(verbose_name='Слаг', max_length=128, unique=True, db_index=True, editable=False)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    '''
+    Переопределение метода сохранения записи.
+    С помощью библиотеки pytils переводим киррилицу в латиницу по полю name.
+    В поле slug записывается поле name на латинице.
+    '''
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+# Модель для источников
+class Source(models.Model):
+    name = models.CharField(verbose_name='Наименование', max_length=128, unique=True)
+    slug = models.SlugField(verbose_name='Слаг', max_length=128, unique=True, db_index=True, editable=False)
+
+    class Meta:
+        verbose_name = 'Источник'
+        verbose_name_plural = 'Источники'
+
+    '''
+    Переопределение метода сохранения записи.
+    С помощью библиотеки pytils переводим киррилицу в латиницу по полю name.
+    В поле slug записывается поле name на латинице.
+    '''
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+# Модель для базовых характеристик продукта
+class Product(models.Model):
+    name = models.CharField(verbose_name='Наименование', max_length=128)
+    name_eng = models.CharField(verbose_name='Наименование (англ.)', max_length=128)
+    slug = models.SlugField(verbose_name='Слаг', max_length=128, unique=True, db_index=True, editable=False)
+    category = models.ForeignKey(verbose_name='Категория', to=Category, on_delete=models.CASCADE)
+    source = models.ForeignKey(verbose_name='Источник', to=Source, on_delete=models.CASCADE)
+    icon_image = models.ImageField(verbose_name='Изображение продукта', upload_to='icons_products')
+    description = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(verbose_name='Цена (руб.)', max_digits=6, decimal_places=2)
+    discount = models.PositiveIntegerField(verbose_name='Скидка (%)', default=0)
+    amount = models.PositiveIntegerField(verbose_name='Количество в наличии', default=0)
+    is_active = models.BooleanField(verbose_name='Активный продукт', default=False)
+
+    class Meta:
+        verbose_name = 'Базовый продукт'
+        verbose_name_plural = 'Базовые продукты'
+
+    '''
+    Переопределение метода сохранения записи.
+    С помощью библиотеки pytils переводим киррилицу в латиницу по полю name.
+    Авто-слаг, при добавлении в конец слага добавляется id.
+    '''
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if not self.slug:
+            self.slug = slugify(self.name) + '-' + str(self.pk)
+            super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+
+# Модель для характеристик продуктов
+class Property(models.Model):
+    name = models.CharField(verbose_name='Наименование', max_length=128)
+
+    class Meta:
+        verbose_name = 'Характеристика продукта'
+        verbose_name_plural = 'Характеристики продуктов'
+
+    def __str__(self):
+        return self.name
+
+
+
+# Модель для сопоставления характеристик продукта и самого продукта
+class ProductProperty(models.Model):
+    product = models.ForeignKey(verbose_name='Продукт', to=Product, on_delete=models.CASCADE)
+    property = models.ForeignKey(verbose_name='Характеристика', to=Property, on_delete=models.CASCADE)
+    value_string = models.CharField(verbose_name='Текстовое значение характеристики', max_length=250, null=True, blank=True, help_text='Значение задается текстом (Автор, обложка, Цвет)')
+    value_integer = models.IntegerField(verbose_name='Числовое значение характеристики', null=True, blank=True, help_text='Значение задается целым числом (Вес книги, год выпуска)')
+    value_genres = models.ManyToManyField(verbose_name='Жанры', to=Genre, null=True, blank=True, help_text='Поле для категорий, где есть жанры (Манга, манхва. маньхуа, ранобэ)')
+
+    class Meta:
+        verbose_name = 'Сопоставление характеристик продуктов'
+        verbose_name_plural = 'Сопоставление характеристик продуктов'
+
+    def __str__(self):
+        return f'{self.product.name} | {self.property.name}'
