2fd4e4bb2ba5b99ee6d312e6381f7ca32488f4f4
katsuyorii <katsuyoriiii@gmail.com>
2024-03-20 12:35:45 +0300

    create registration user view and template

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import User
 
 # Класс-форма для авторизации пользователя
 class LoginUserForm(forms.Form):
@@ -8,6 +9,29 @@ class LoginUserForm(forms.Form):
     }))
 
     password = forms.CharField(widget=forms.PasswordInput(attrs= {
+        'class': 'login-email-input', 
+        'placeholder': 'Введите пароль',
+    }))
+
+
+# Класс-форма для регистрации нового пользователя
+class RegistrationUserForm(forms.Form):
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'login-email-input', 
+        'placeholder': 'Введите email',
+    }))
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'login-email-input', 
+        'placeholder': 'Введите email',
+    }))
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs= {
+        'class': 'login-email-input', 
+        'placeholder': 'Введите пароль',
+    }))
+    
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs= {
         'class': 'login-email-input', 
         'placeholder': 'Введите пароль',
     }))
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -2,8 +2,9 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
 from django.views.generic import FormView
-from .forms import LoginUserForm
+from .forms import LoginUserForm, RegistrationUserForm
 from django.contrib import messages
+from .models import User
 
 
 # Класс-представление авторизации пользователя
@@ -30,3 +31,30 @@ class LoginUserView(FormView):
 
         return context
 
+
+# # Класс-представление регистрации нового пользователя
+class RegistrationUserView(FormView):
+    form_class = RegistrationUserForm
+    template_name = 'users/registration.html'
+
+    def form_valid(self, form): 
+        username = form.cleaned_data['username']
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
+        password2 = form.cleaned_data['password2']
+
+        if password == password2:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            login(self.request, user)
+        
+            messages.success(self.request, 'Вы зарегестрировались в системе!')
+            return HttpResponseRedirect(reverse_lazy('index'))
+        else:
+            return self.form_invalid(form)
+            
+
+    def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context['title'] = 'Авторизация'
+
+            return context
