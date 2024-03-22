5b7cf0e871424bb35751d7481d01c013dfdc1935
katsuyorii <katsuyoriiii@gmail.com>
2024-03-22 07:24:38 +0300

    fix favorites ajax js

--- mysite/static/js/forever.js
+++ mysite/static/js/forever.js
@@ -9,9 +9,9 @@ $(document).ready(function() {
             type: 'GET', // Метод запроса GET, так как мы просто добавляем в избранное
             success: function(response) {
                 // Когда запрос успешно выполнен, меняем цвет текста ссылки и заменяем изображение
-                link.css('color', 'red'); // Изменяем цвет текста ссылки на красный
-                link.text('В избранном')
-                $('#forever_id').attr('src', '/static/img/icons/heart-red.png');
+                link.css('color', response.button_color); // Изменяем цвет текста ссылки на красный
+                link.text(response.button_text)
+                $('#forever_id').attr('src', response.new_img);
             },
             error: function(xhr, errmsg, err) {
                 // Обработка ошибок, если что-то пошло не так
