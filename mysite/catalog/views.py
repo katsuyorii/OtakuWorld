f23dfe2c3ee6d94386eb24b502dad353e26c79a5
katsuyorii <katsuyoriiii@gmail.com>
2024-03-23 19:23:59 +0300

    refactoring catalog start

--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -1,16 +1,20 @@
-from django.http import JsonResponse
-from django.urls import reverse_lazy
-from django.views.generic.list import ListView
 from django.views.generic.base import View
-from .models import Category, Product, ProductProperty, Comment
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic import UpdateView
 from django.views.generic.edit import FormMixin
-from django.shortcuts import get_object_or_404, redirect
+
+from django.http import HttpResponseRedirect, JsonResponse
+from django.urls import reverse_lazy
+
+from django.db import transaction
+from .models import Category, Product, ProductProperty, Comment, Favorites
+
 from .forms import AddNewCommentForm, EditCommentForm
+
+from django.shortcuts import get_object_or_404, redirect
 from django.contrib import messages
 from django.contrib.auth.models import AnonymousUser
-from django.db import transaction
-from .models import Favorites
 
 
 # Класс-представления каталога категорий
@@ -41,38 +45,37 @@ class ProductListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Каталог товаров'
-        context['category'] = get_object_or_404(Category, slug=self.kwargs['category_slug'])
+        context['selected_category'] = get_object_or_404(Category, slug=self.kwargs['category_slug'])
 
         return context
     
 
 # Класс-представление для отдельного товара
-class ProductDetailView(ListView, FormMixin):
+class ProductDetailView(DetailView, FormMixin):
     model = Product
     form_class = AddNewCommentForm
     template_name = 'catalog/product-detail.html'
     context_object_name = 'product'
+    slug_url_kwarg = 'product_slug'
 
     def get_queryset(self):
-        queryset = {
-            'base': get_object_or_404(Product.objects.select_related('category', 'source'), slug=self.kwargs['product_slug']),
-            'characters': ProductProperty.objects.filter(product__slug=self.kwargs['product_slug']).select_related('product', 'property'),
-            'comments': Comment.objects.filter(product__slug=self.kwargs['product_slug']).select_related('user').order_by('-create_date'),
-        }
+        queryset = Product.objects.filter(slug=self.kwargs['product_slug']).select_related('category', 'source')
 
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = self.object_list['base'].name
+        context['title'] = self.object.name
+        context['proretries'] = ProductProperty.objects.filter(product__slug=self.kwargs['product_slug']).select_related('property')
+        context['comments'] = Comment.objects.filter(product__slug=self.kwargs['product_slug']).select_related('user').order_by('-create_date')
         context['count_reviews'] = Comment.objects.filter(product__slug = self.kwargs['product_slug']).count()
 
         # Проверка, если пользователь не авторизирован, то у него нет доступа к форме создания комментария и добавления в избранное
         if isinstance(self.request.user, AnonymousUser):
-            context['user_comm'] = True
+            context['is_user_comment'] = True
             context['is_favorites'] = False
         else:
-            context['user_comm'] = Comment.objects.filter(user=self.request.user, product__slug=self.kwargs['product_slug']).exists()
+            context['is_user_comment'] = Comment.objects.filter(user=self.request.user, product__slug=self.kwargs['product_slug']).exists()
             context['is_favorites'] = Favorites.objects.filter(user=self.request.user, product__slug = self.kwargs['product_slug']).exists()
 
         return context
@@ -83,14 +86,11 @@ class ProductDetailView(ListView, FormMixin):
     # Метод добавления комментариев
     @transaction.atomic
     def post(self, request, *args, **kwargs):
-        # -------- НЕ ТРОГАТЬ -----------------
-        self.object_list = self.get_queryset()
-        # -------------------------------------
-
         form = self.get_form()
+
         if form.is_valid():
             new_comment = form.save(commit=False)
-            new_comment.product = get_object_or_404(Product.objects.select_related('category', 'source'), slug=self.kwargs['product_slug'])
+            new_comment.product = get_object_or_404(Product, slug=self.kwargs['product_slug'])
             new_comment.user = self.request.user
             new_comment.save()
             new_comment.product.update_rating()
@@ -124,10 +124,10 @@ class CommentDeleteView(View):
 # Класс-представление для редактирования комментария
 class CommentEditView(UpdateView):
     model = Comment
-    template_name = 'catalog/review-edit.html'
-    pk_url_kwarg = 'comment_id'
     form_class = EditCommentForm
-    context_object_name = 'comm'
+    template_name = 'catalog/review-edit.html'
+    context_object_name = 'comment'
+    pk_url_kwarg = 'comment_id'
 
     def get_queryset(self):
         queryset = Comment.objects.filter(pk=self.kwargs['comment_id']).select_related('product__category')
@@ -138,8 +138,9 @@ class CommentEditView(UpdateView):
     def form_valid(self, form):
         self.object = form.save()
         self.object.product.update_rating()
+        
         messages.success(self.request, 'Ваш комментарий успешно редактирован!')
-        return super().form_valid(form)
+        return HttpResponseRedirect(self.get_success_url())
     
     def form_invalid(self, form):
         messages.error(self.request, 'Ошибка при заполнении формы!')
